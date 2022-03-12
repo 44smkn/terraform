00e85bc1ed787815f0cc4dd639d787c800df84d6
44smkn <jrywm121@gmail.com>
2022-03-12 10:29:58 +0900

    Add aws temlate for tfaction

---
+++ templates/aws/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  backend "s3" {
+    region = "ap-northeast-1"
+    bucket = "v1-44smkn-terraform-state"
+    key    = "%%TARGET%%/v1/terraform.tfstate"
+  }
+
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.0.0"
+    }
+  }
+}
+
+provider "aws" {
+  region      = local.region
+  max_retries = 3
+}
