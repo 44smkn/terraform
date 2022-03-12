01665cff33a26ab2901095ace34e729af4da5d45
44smkn <jrywm121@gmail.com>
2022-03-13 00:53:28 +0900

    Change argument name

--- modules/eks-with-karpenter/main.tf
+++ modules/eks-with-karpenter/main.tf
@@ -35,11 +35,11 @@ module "eks" {
   cluster_version = "1.21"
   cluster_name    = var.cluster_name
   vpc_id          = module.vpc.vpc_id
-  subnets         = module.vpc.private_subnets
+  subnet_ids      = module.vpc.private_subnets
   enable_irsa     = true
 
   # Only need one node to get Karpenter up and running
-  worker_groups = [
+  self_managed_node_groups = [
     {
       instance_type = "t3a.medium"
       asg_max_size  = 1
