1d691c9efa1efc03e286aba9288281f6bb684304
44smkn <jrywm121@gmail.com>
2022-03-13 00:46:28 +0900

    upgrade module version

--- aws/eks/main.tf
+++ aws/eks/main.tf
@@ -20,6 +20,6 @@ provider "aws" {
 }
 
 module "eks" {
-  source       = "github.com/44smkn/terraform//modules/eks-with-karpenter?ref=module_modules_eks-with-karpenter_v0.1.0"
+  source       = "github.com/44smkn/terraform//modules/eks-with-karpenter?ref=module_modules_eks-with-karpenter_v0.2.1"
   cluster_name = "44smkn-test"
 }
